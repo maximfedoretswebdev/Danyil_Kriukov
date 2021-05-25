a010b6705a924ead5c8dc2a979d1a0b72340669f
maximfedoretswebdev <69167344+maximfedoretswebdev@users.noreply.github.com>
2021-05-24 20:31:52 -0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -3,3 +3,8 @@ var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 2000,
   touch: true
 })
+
+
+function displayAlerts() {
+    alert("Gallery not available yet, please come back later.");
+}
